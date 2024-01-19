7e018f248741a38c363cc21ead51d9258bde0813
devcarolinealmeida <carolineitalia85@gmail.com>
2024-01-19 11:03:49 +0100

    add conditional if

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const start = document.getElementById('btn-start');
 
 start.addEventListener('click', () => {
     for (i = 1; i <= 100; i++) {
-        console.log(i);
+        if(i % 3 == 0 && i % 5 == 0) {
+            console.log('foobar');
+        }
+        else if (i % 3 == 0) {
+            console.log('foo');
+        }
+        else if (i % 5 == 0) {
+            console.log('bar');
+        }
+        else {
+            console.log(i);
+        }
     }
 })
